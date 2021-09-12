dd75f3f1c2a3546c733319b33243fbc23d04e9b1
Timothy Luna <timothy.d.luna@gmail.com>
2021-09-12 16:03:16 -0500

    Adding base kd tree with api
    This commit adds the skeleton for the tree API including documentation stubs.

---
+++ adaptive_kd_tree/kdtree.py
@@ -0,0 +1,32 @@
+import scipy.spatial as sns
+
+class AdaptiveTree():
+  """
+  """
+  def __init__():
+    pass
+  
+  def Load(self, dataset, embedding_function):
+    """
+    """
+    pass
+  
+  def Build(self, sample_function):
+    """
+    """
+    pass
+  
+  def Sample(self, location, n, r):
+    """
+    """
+    pass
+  
+  def UpdateEmbeddings(self, embeddings, gradient):
+    """
+    """
+    pass
+  
+  def Embeddings(self, embeddings):
+    """
+    """
+    pass
